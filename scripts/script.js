8b21a0bd20ec9f4f1988c36e489ee2edc4b6c9bb
Chase Huber <chasehuber23@gmail.com>
2022-11-22 18:36:09 -0500

    added the ability to type text into the text area, a little janky but it's working right now.renamed shape.html and second css file to game-style.css

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,36 @@
-function createLog() {
-    // creates the log where player will read output
+let input = document.getElementById("input-box");
+let outputLog = document.getElementById("output-box");
+
+function addingEventListener() {
+    const inputBtn = document.getElementById("input-btn");
+
+    function clickAlert() {
+        alert("I was clicked!");
+    }
+
+    input.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            updateOutputLog();
+        }
+    });
+
 }
 
-function input() {
-    // handles player input
+function updateOutputLog() {
+    if (outputLog.value != "") {
+        outputLog.value = `${outputLog.value}\n>${input.value}`;
+        scrollOutputToBottom();
+        input.value = "";
+    }
+    else {
+        outputLog.value = `>${input.value}`;
+        input.value = "";
+    }
 }
 
-function output() {
-    // handles log output
+function scrollOutputToBottom() {
+    outputLog.scrollTop = outputLog.scrollHeight;
 }
+
+addingEventListener();
