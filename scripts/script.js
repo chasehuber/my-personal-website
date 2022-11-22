49e5c53c745dd2dcda8ef3883d3c90da905fcabb
Chase Huber <chasehuber23@gmail.com>
2022-11-22 17:11:49 -0500

    fixed some code in index.html, shape.html, and script.js. added some folders for better organization.

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function createLog() {
+    // creates the log where player will read output
+}
+
+function input() {
+    // handles player input
+}
+
+function output() {
+    // handles log output
+}
